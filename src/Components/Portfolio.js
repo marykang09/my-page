228593f6e49a08f9ffa404b34fb61420af8b567a
marykang09 <mary.kang09@gmail.com>
2020-10-27 15:36:52 -0400

    add project

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -31,7 +31,7 @@ class Portfolio extends Component {
 
             <h1>Check Out Some of My Projects</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
             </div>
           </div>
